494445defa10cd6335e26ea570c7ddb79bded036
lindmark421@gmail.com <lindmark421@gmail.com>
2019-09-06 22:08:39 -0400

    Updated Controllers

---
+++ PRSDatabaseProject/Controllers/RequestLinesAPIController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PRSDatabaseProject.Models;
+
+namespace PRSDatabaseProject.Controllers
+{
+    [Route("api/RequestLines")]
+    [ApiController]
+    public class RequestLinesAPIController : ControllerBase
+    {
+        private readonly PRSDb _context;
+
+        public RequestLinesAPIController(PRSDb context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RequestLinesAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RequestLine>>> GetRequestLines()
+        {
+            return await _context.RequestLines.ToListAsync();
+        }
+
+        // GET: api/RequestLinesAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RequestLine>> GetRequestLine(int id)
+        {
+            var requestLine = await _context.RequestLines.FindAsync(id);
+
+            if (requestLine == null)
+            {
+                return NotFound();
+            }
+
+            return requestLine;
+        }
+
+        // PUT: api/RequestLinesAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRequestLine(int id, RequestLine requestLine)
+        {
+            if (id != requestLine.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(requestLine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RequestLineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RequestLinesAPI
+        [HttpPost]
+        public async Task<ActionResult<RequestLine>> PostRequestLine(RequestLine requestLine)
+        {
+            _context.RequestLines.Add(requestLine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRequestLine", new { id = requestLine.Id }, requestLine);
+        }
+
+        // DELETE: api/RequestLinesAPI/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RequestLine>> DeleteRequestLine(int id)
+        {
+            var requestLine = await _context.RequestLines.FindAsync(id);
+            if (requestLine == null)
+            {
+                return NotFound();
+            }
+
+            _context.RequestLines.Remove(requestLine);
+            await _context.SaveChangesAsync();
+
+            return requestLine;
+        }
+
+        private bool RequestLineExists(int id)
+        {
+            return _context.RequestLines.Any(e => e.Id == id);
+        }
+    }
+}
